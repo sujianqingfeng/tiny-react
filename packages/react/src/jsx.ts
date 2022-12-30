a37b4a7002fab03fb16688cc1f044afe050a8b4d
hens <121116111@qq.com>
2022-12-30 21:31:17 +0800

    feat: runtime jsx

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,50 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols'
+import type { ElementType, Key, Props, ReactElement, Ref, Type } from 'shared/ReactTypes'
+
+const ReactElement = function(type: Type, key: Key, ref: Ref, props: Props): ReactElement {
+  const element = {
+    $$typeof: REACT_ELEMENT_TYPE,
+    type,
+    key,
+    ref,
+    props,
+    __mark: 'white-letter'
+  }
+  return element
+}
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+
+  const props: Props = {}
+  let key: Key = null
+  let ref: Ref = null
+
+  for (const prop of config) {
+    const val = config[prop]
+    if (prop === 'key') {
+      key = `${val}`
+    }
+    if (prop === 'ref') {
+      if (val !== undefined) {
+        ref = val
+      }
+    }
+    if (Object.prototype.hasOwnProperty.call(config, prop)) {
+      props[prop] = val
+    }
+  }
+
+  const maybeChildrenLength = maybeChildren.length
+
+  if (maybeChildrenLength ) {
+    if (maybeChildrenLength === 1) {
+      props.children = maybeChildren[0]
+    } else {
+      props.children = maybeChildren
+    }
+  }
+
+  return ReactElement(type, key, ref, props)
+}
+
+export const jsxDev = jsx
