aef85c928b5511c5e09138e9f58e2af80263a480
hens <121116111@qq.com>
2023-01-03 00:10:46 +0800

    feat: rollup pack

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,50 @@
+import generatePackageJson from 'rollup-plugin-generate-package-json'
+import { defineConfig } from 'rollup'
+
+import { getBaseRollupPlugins, getPackageJson, resolvePkgPath } from './utils'
+
+const { name, module } = getPackageJson('react')
+const pkgPath = resolvePkgPath(name)
+const pkgDistPath = resolvePkgPath(`${name}/node_modules`, true)
+
+export default defineConfig([
+  // react
+  {
+    input: `${pkgPath}/${module}`,
+    output: {
+      file: `${pkgDistPath}/index.js`,
+      name: 'index.js',
+      format: 'umd'
+    },
+    plugins: [
+      ...getBaseRollupPlugins(),
+      generatePackageJson({
+        inputFolder: pkgPath,
+        outputFolder: pkgDistPath,
+        baseContents: ({ name, description, version }) => ({
+          name,
+          version,
+          description,
+          main: 'index.js'
+        })
+      })
+    ]
+  },
+  // jsx-runtime
+  {
+    input: `${pkgPath}/src/jsx.ts`,
+    output: [
+      {
+        file: `${pkgDistPath}/jsx-runtime.js`,
+        name: 'jsx-runtime.js',
+        format: 'umd'
+      },
+      {
+        file: `${pkgDistPath}/jsx-dev-runtime.js`,
+        name: 'jsx-devruntime.js',
+        format: 'umd'
+      }
+    ],
+    plugins: getBaseRollupPlugins()
+  }
+])
---
+++ scripts/rollup/utils.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import fs from 'fs'
+import cjs from '@rollup/plugin-commonjs'
+import ts from 'rollup-plugin-typescript2'
+
+const pkgPath = path.resolve(__dirname, '../../packages')
+const distPath = path.resolve(__dirname, '../../dist')
+
+export function resolvePkgPath(pkgName, isDist) {
+  if (isDist) {
+    return `${distPath}/${pkgName}`
+  }
+  return `${pkgPath}/${pkgName}`
+}
+
+export function getPackageJson(pkgName) {
+  const jsonPath = resolvePkgPath(`${pkgName}/package.json`)
+  const result = fs.readFileSync(jsonPath, 'utf-8')
+  return JSON.parse(result)
+}
+
+export function getBaseRollupPlugins({ typescript } = {}) {
+  return [cjs(), ts(typescript)]
+}
