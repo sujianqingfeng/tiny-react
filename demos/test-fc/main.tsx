fa8fe57300a2c349f443c114ce4795c6da9dd659
hens <121116111@qq.com>
2023-01-28 23:32:10 +0800

    feat: add demo

---
+++ demos/test-fc/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <span>app</span>
+}
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <App />
+)
