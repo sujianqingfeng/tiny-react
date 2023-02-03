622f3fc8784c3eacb7b78faeadaa0da3c3cc4129
hens <121116111@qq.com>
2023-02-03 22:18:17 +0800

    feat: render commitWork part

--- packages/react-dom/src/hostConfig.ts
+++ packages/react-dom/src/hostConfig.ts
@@ -1,5 +1,9 @@
+import { FiberNode } from 'react-reconciler/src/fiber'
+import { HostText } from 'react-reconciler/src/workTags'
+
 export type Container = Element
 export type Instance = Element
+export type TextInstance = Text
 
 export const createInstance = (type: string) => {
   const element = document.createElement(type)
@@ -15,4 +19,29 @@ export const createTextInstance = (content: string) => {
 }
 
 export const appendChildToContainer = appendInitialChild 
+
+export function commitUpdate(fiber: FiberNode) {
+
+  switch (fiber.tag) {
+    case HostText:
+      // eslint-disable-next-line no-case-declarations
+      const text = fiber.memoizedProps.content
+      return commitTextUpdate(fiber.stateNode, text)
+  
+    default:
+      if (__DEV__) {
+        console.warn('commit Update 未实现的tag', fiber)
+      }
+      break
+  }
+
+}
+
+export function commitTextUpdate(textInstance: TextInstance, content: string) {
+  textInstance.textContent = content
+}
+
+export function removeChild(child: Instance | TextInstance, container: Container) {
+  container.removeChild(child)
+}
 
--- packages/react-reconciler/src/commitWork.ts
+++ packages/react-reconciler/src/commitWork.ts
@@ -1,7 +1,7 @@
-import { appendChildToContainer, Container } from 'hostConfig'
+import { appendChildToContainer, commitUpdate, Container, removeChild } from 'hostConfig'
 import { FiberNode, FiberRootNode } from './fiber'
-import { MutationMask, NoFlags, Placement } from './fiberFlags'
-import { HostComponent, HostRoot, HostText } from './workTags'
+import { ChildDeletion, MutationMask, NoFlags, Placement, Update } from './fiberFlags'
+import { FunctionComponent, HostComponent, HostRoot, HostText } from './workTags'
 
 let nextEffect: FiberNode| null = null
 
@@ -36,6 +36,95 @@ function commitMutationEffectsOnFiber(finishedWork: FiberNode) {
   if ((flags & Placement) !== NoFlags) {
     commitPlacement(finishedWork)
     finishedWork.flags &= ~Placement
+  }
+
+  if ((flags & Update) !== NoFlags) {
+    commitUpdate(finishedWork)
+    finishedWork.flags &= ~Update
+  }
+
+  if ((flags & ChildDeletion) !== NoFlags) {
+    const deletions = finishedWork.deletions
+    if (deletions !== null) {
+      deletions.forEach(childToDelete => {
+        commitDeletion(childToDelete)
+      })
+
+    }
+    finishedWork.flags &= ~ChildDeletion
+  }
+}
+
+function commitDeletion(childToDelete: FiberNode) {
+  let rootHostNode: FiberNode | null = null 
+
+  commitNestedComponent(childToDelete, (unmountFiber) => {
+
+    switch (unmountFiber.tag) {
+      case HostComponent:
+        if (rootHostNode === null) {
+          rootHostNode = unmountFiber
+        }
+        //TODO 解绑ref
+        return 
+
+      case HostText:
+        if (rootHostNode === null) {
+          rootHostNode = unmountFiber
+        }
+        return
+      case FunctionComponent:
+        // TODO useEffect unmount
+        return
+    
+      default:
+        if (__DEV__) {
+          console.warn('未处理的unmount类型', unmountFiber)
+        }
+        break
+    }
+  })
+
+  if (rootHostNode !== null) {
+    const hostParent = getHostParent(childToDelete)
+    if (hostParent !== null) {
+      removeChild(rootHostNode, hostParent)
+    }
+  }
+
+  // 重置一些数据
+  childToDelete.return = null
+  childToDelete.child = null
+}
+
+function commitNestedComponent(root: FiberNode, onCommitUnmount: (fiber: FiberNode) => void) {
+  let node = root
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    onCommitUnmount(node)
+
+    // 向下遍历
+    if (node.child !== null) {
+      node.child.return = node
+      node = node.child
+      continue
+    }
+
+    // 递归结束
+    if (node === root) {
+      return
+    }
+
+    while (node.sibling === null) {
+      // 父级遍历结束
+      if (node.return === null || node.return === root) {
+        return
+      }
+      node = node.return
+    }
+
+    node.sibling.return = node.return
+    node = node.sibling
   }
 }
 
